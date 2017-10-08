6e8c953db51d27b48c9b0f47d558d356974833a9
Alexandr Ukhanov <alexandrukho@gmail.com>
2017-10-08 15:16:57 +0300

    hw1_task7

---
+++ hw1/task7.php
@@ -0,0 +1,20 @@
+<?php
+
+
+echo "<table>";
+echo "<tbody>";
+for ($rows = 1; $rows <= 10; $rows++) {
+    echo "<tr style='text-align: center;'>";
+    for ($cols = 1; $cols <= 10; $cols++) {
+        if ($rows % 2 == 0 && $cols % 2 == 0) {
+            echo "<td>" . "(" . $rows * $cols . ")" . "</td>";
+        } else if ($rows % 2 == 1 && $cols % 2 == 1) {
+            echo "<td>" . "[" . $rows * $cols . "]" . "</td>";
+        } else {
+            echo "<td>" . $rows * $cols . "</td>";
+        }
+    }
+    echo "</tr>";
+}
+echo "</tbody>";
+echo "</table>";
