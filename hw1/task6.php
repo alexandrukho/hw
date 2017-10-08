9b0d438b8c356634a6757585df0462b0d5ca1ded
Alexandr Ukhanov <alexandrukho@gmail.com>
2017-10-08 11:18:53 +0300

    hw1_task6

---
+++ hw1/task6.php
@@ -0,0 +1,47 @@
+<?php
+$bmw["bmw"] = [
+    "model" => "X5",
+    "speed" => "120",
+    "doors" => "5",
+    "year" => "2015"
+];
+$toyota["toyota"] = [
+    "model" => "Camry",
+    "speed" => "210",
+    "doors" => "4",
+    "year" => "2011"
+];
+$opel["opel"] = [
+    "model" => "Astra",
+    "speed" => "220",
+    "doors" => "2",
+    "year" => "2013"
+];
+
+$cars = [
+    "bmw" => [
+        "model" => "X5",
+        "speed" => "120",
+        "doors" => "5",
+        "year" => "2015"
+    ],
+    "toyota" => [
+        "model" => "Camry",
+        "speed" => "210",
+        "doors" => "4",
+        "year" => "2011"
+    ],
+    "opel" => [
+        "model" => "Astra",
+        "speed" => "220",
+        "doors" => "3",
+        "year" => "2013"
+    ]
+];
+
+foreach ($cars as $brand => $keys) {
+    echo "<br> <span>Car:</span> $brand <br>";
+    foreach ($keys as $innerKeys => $value) {
+        echo " " . "$value";
+    }
+}
