022e65e38b54817a6adea825fdaf5bf704dc280b
Alexandr Ukhanov <alexandrukho@gmail.com>
2017-10-12 16:48:47 +0300

    hw2_task9

--- hw2/index.php
+++ hw2/index.php
@@ -16,4 +16,6 @@ echo task6();
 echo "<br>";
 echo task7('Карл у Клары украл Кораллы');
 echo "<br>";
-echo task8("RX packets:9132 errors:0 dropped:0 :) overruns:0 frame:0. ");
+echo task8("RX packets:9132 errors:0 dropped:0 :)  overruns:0 frame:0. ");
+echo "<br>";
+echo task9('test.txt');
